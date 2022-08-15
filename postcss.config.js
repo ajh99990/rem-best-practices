38817a2b734a60f1832dc4c80ea4c4230e9415d5
ajh99990 <nduonduo@qq.com>
2022-08-15 16:43:16 +0800

    docs：修改文档

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require("postcss-pxtorem")({
       rootValue: 1,
-      propList: ['*', '!font-size'],
+      propList: ['*', '!font-size'],//字体不需要参与缩放
     })
   ],
 };
--- src/autoSize.js
+++ src/autoSize.js
@@ -1,16 +1,16 @@
 import { isMobile } from "./helper"
 import _ from 'lodash'
 
-//方案1:根据ua设置字体大小
+//方案1:我这里是根据ua来区分移动端和pc端,您可以选择适合自己的方案
 export function setDomFontSizeByUa() {
   const isMobileDev = isMobile()
   let fontsize = '';
   let width = document.documentElement.clientWidth || document.body.clientWidth;
   if (isMobileDev) {
-    fontsize = width / 375 + 'px'
+    fontsize = width / 375 + 'px' //重点在这,375是移动端设计稿的宽度
   }
   else {
-    fontsize = width / 1920 + 'px'
+    fontsize = width / 1920 + 'px' //重点在这,1920是pc端设计稿的宽度
   }
   (document.getElementsByTagName('html')[0].style)['font-size'] = fontsize;
 }
